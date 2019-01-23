77750953c4f1b8512fb8a4a7ae19c94d17969cc6
Cynthia <cturner@ltu.edu>
2019-01-22 22:08:38 -0500

    saved

--- src/lab7/ThisIsALot.java
+++ src/lab7/ThisIsALot.java
@@ -8,52 +8,62 @@ public class ThisIsALot {
 		// TODO Auto-generated method stub
 		Scanner scnr = new Scanner(System.in);
 
-		String nameValid = " ";
-//		String email = " ";
-
-		System.out.println("Enter a name: ");
-		nameValid = scnr.next();
+		String name = readName(scnr,"Enter your first name: ", 0, 30);
+		System.out.println("Congratulations! You are " + name + "!");
 		
-		System.out.println(nameValid);
-//		
-//		do {
-//			name = scnr.nextLine();
-//			//Valid Java identifier: [a-zA-Z_$][\w$]
-//			boolean valid = name.matches("([A-Z][A-Za-z])+");
-//			System.out.println(valid ? "Valid!" : "Invalid :(");
-//			
-//		}while (!name.isEmpty());
-//		System.out.println("Thanks for playing.");
-//		
+		
+		int two = readInt(scnr, "What is gravity?: ", 0.0, 10.0);
+		System.out.println("YOU'RE GROUNDED!");
+		
 		scnr.close();
 	}
-
-	private static void nameValid(String nameValid) {
-		boolean valid = nameValid.matches("([A-Z]{1}[a-z]+\\s*([A-Z]{1}[a-z]+)*){1,30}");
-		System.out.println(valid ? "Valid!" : "Invalid :(");
-		
-	}
-		
-
-//	private static boolean emailValid(String email) {
-//			if (i%3 == 0) {
-//				return true;
-//			}else {
-//				return false;
-//			}
+	private static boolean valid(String input) {
+        return (input.matches("[A-Z][a-zA-z]*"));
+        }
+    private static boolean validEmail(String input) {
+        return (input.matches("[a-zA-Z$_0-9]{5,30}@[a-zA-Z]{5,10}.[a-zA-Z]{2,3}"));
+    }
+}
+//	public static String readName(Scanner scnr, String prompt) {
+//		String name = readName(scnr, prompt);
+//		while (name < i || name > j) {
+//			//invalid!!
+//			System.out.println("The number must be in the range " + i + " to " + j + ". Try again.");
+//			name = readString(scnr, prompt);
+//		}
+//		return name;
 //	}
-//	private static boolean phoneValid(int i) {
-//				if (i%3 == 0) {
-//					return true;
-//				}else {
-//					return false;
-//				}
+//	public static String readString(Scanner scnr, String prompt) {
+//		System.out.println(prompt);
+//		while (!scnr.hasNextLine()) {
+//			scnr.next();//clears the scanner token
+//			System.out.println("Invalid integer. Try again.");
+//			System.out.println(prompt);
+//		}
+//		String email = scnr.toString();
+//		scnr.nextLine();
+//		return email;
+//		
 //	}
-//	private static boolean dateValid(int i) {
-//					if (i%3 == 0) {
-//						return true;
-//					}else {
-//						return false;
-//					}
-	}
-	
+//	public static int readInt(Scanner scnr, String prompt) {
+//		System.out.println(prompt);
+//		while (!scnr.hasNextInt()) {
+//			scnr.next();// clears the scanner token
+//			System.out.println("Invalid integer. Try again.");
+//			System.out.println(prompt);
+//		}
+//		int two = scnr.nextInt();
+//		scnr.nextLine();
+//		return two;
+//	}
+//
+//	public static int readInt(Scanner scnr, String prompt, int min, int max) {
+//		int two = readInt(scnr, prompt);
+//		while (two < min || two > max) {
+//			// invalid!!
+//			System.out.println("The number must be in the range " + min + " to " + max + ". Try again.");
+//			two = readInt(scnr, prompt);
+//		}
+//		return two;
+//	}
+}
